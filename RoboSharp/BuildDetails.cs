270341c7838dfa55dcf4fb283e5c359b8844779d
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-02-28 09:23:18 +0100

    Add raw version of build-details with dummy values.

---
+++ RoboSharp/BuildDetails.cs
@@ -0,0 +1,11 @@
+public static class BuildDetails
+{
+    public const string GitCommitName = "unknown";
+    public const string GitBranch = "unknown";
+    public const string GitMajor = "1";
+    public const string GitMinor = "0";
+    public const string GitPatch = "0";
+    public const string GitTagDist = "0";
+    public const string BuildDate = "unknown";
+    public const string GitCommit = "unknown";
+}
