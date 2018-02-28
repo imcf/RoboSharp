e74a5b1d87b5704d4819cae3a5abc7c76081c14a
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-02-28 08:49:18 +0100

    Add description to the assembly.

--- RoboSharp/Properties/AssemblyInfo.cs
+++ RoboSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RoboSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# wrapper for RoboCopy")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RoboSharp")]
