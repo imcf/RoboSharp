1ab21c01d700fa2e4af07a309ab9d36c5bd6b371
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-02-28 09:24:34 +0100

    Feed assembly info with provided details.

--- RoboSharp/Properties/AssemblyInfo.cs
+++ RoboSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RoboSharp")]
-[assembly: AssemblyDescription("C# wrapper for RoboCopy")]
+[assembly: AssemblyDescription("C# wrapper for RoboCopy (" + BuildDetails.GitCommit + ")")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RoboSharp")]
@@ -32,5 +32,15 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(BuildDetails.GitMajor + "." +
+                           BuildDetails.GitMinor + "." +
+                           BuildDetails.GitPatch + "." +
+                           BuildDetails.GitTagDist)]
+[assembly: AssemblyFileVersion(BuildDetails.GitMajor + "." +
+                               BuildDetails.GitMinor + "." +
+                               BuildDetails.GitPatch + "." +
+                               BuildDetails.GitTagDist)]
+
+[assembly: AssemblyInformationalVersion(BuildDetails.BuildDate +
+                                        " " + BuildDetails.GitCommit +
+                                        " (" + BuildDetails.GitBranch + ")")]
